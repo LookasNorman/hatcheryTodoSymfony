52ef1be0329f7c76fa329b3f7773b68e003461a5
LookasNorman <lookasziebice@gmail.com>
2020-12-29 18:37:28 +0100

    Added CRUD for machines entity

---
+++ src/Form/MachinesType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Machines;
+use App\Entity\ObjectAddress;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MachinesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('objectAddress', EntityType::class, [
+                'class' => ObjectAddress::class,
+                'choice_label' => 'name',
+                'attr' => [
+                    'class' => 'form-select'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Machines::class,
+        ]);
+    }
+}
